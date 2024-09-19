eae350341312e02e531564b8410a16160659337d
Aditya Chakma <aditya.chakma@therapservices.net>
2024-09-19 08:45:24 +0600

    Added facebook Reels remove support

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Attention!
+<hl>
+
+<ul>
+<li>Removes youtube shorts from feed</li>
+<li>Removes facebook reels from feed</li>
+</ul>
--- contentScript.js
+++ contentScript.js
@@ -2,6 +2,9 @@
   removeShorts();
   setInterval(removeShorts, 2000);
 
+  removeReels();
+  setInterval(removeReels, 2000);
+
   function removeShorts() {
     shorts = document.querySelectorAll("ytd-rich-section-renderer");
 
@@ -11,6 +14,16 @@
       console.log(
         "Attention extension has removed " + shorts.length + " shorts"
       );
+    }
+  }
+
+  function removeReels() {
+    reels = document.querySelectorAll('[aria-label="Reels"]');
+
+    if (reels && reels.length > 0) {
+      reels.forEach((el) => el.remove());
+
+      console.log("Attention extension has removed " + reels.length + " reels");
     }
   }
 })();
