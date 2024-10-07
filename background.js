e46f63e18ba93786505cf9ea553a7d4378a5fa8f
Aditya Chakma <aditya.chakma@therapservices.net>
2024-10-07 16:21:41 +0600

    Adding bookmarks

--- background.js
+++ background.js
@@ -6,8 +6,15 @@ chrome.tabs.onUpdated.addListener((tabId, _, tab) => {
     if (tab.url && tab.url.includes("youtube.com")) {
         tabUrl = "youtube.com";
         properties["url"] = tab.url;
+
         if (tab.url.includes("watch")) {
             properties["source"] = "yt-watch";
+
+            const queryParameters = tab.url.split("?")[1];
+            const urlParameters = new URLSearchParams(queryParameters);
+            properties["videoId"] = urlParameters.get("v");
+
+            console.log("url parameters: " + urlParameters);
         } else {
             properties["source"] = "yt-home";
         }
--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,57 @@
 (() => {
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+    let intervalId = "";
+    let currentVideoBookmarks = [];
+
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const { type, source } = obj;
 
         if (source === "fb") {
             removeReels();
-            setInterval(removeReels, 2000);
+            intervalId = setInterval(removeReels, 2000);
         } else if (source === "yt-home" || source === "yt-watch") {
+            if (source === "yt-watch") {
+                bookMark(obj);
+            }
             removeShorts(source);
-            setInterval(removeShorts, 2000);
+            intervalId = setInterval(removeShorts, 2000);
         }
     });
+
+    function bookMark(obj) {
+        const { source, videoId } = obj;
+
+        if (source !== "yt-watch") {
+            return;
+        }
+
+        currentVideo = videoId;
+        newVideoLoaded();
+    }
+
+    const newVideoLoaded = async () => {
+        const bookmarkBtnExist = document.getElementsByClassName("bookmark-btn")[0];
+
+        console.log("yt-watch content");
+
+        if (!bookmarkBtnExist) {
+            const bookmarkBtn = document.createElement("img");
+
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+            youtubeLeftControls.appendChild(bookmarkBtn);
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+        }
+    };
+
+    // call on page reload
+    //newVideoLoaded();
 
     function removeShorts(source) {
         shorts =
@@ -33,4 +75,25 @@
             console.log("Attention extension has removed " + reels.length + " reels");
         }
     }
+
+    const addNewBookmarkEventHandler = () => {
+        const currentTime = youtubePlayer.currentTime;
+        const newBookmark = {
+            time: currentTime,
+            desc: "Bookmark at " + currentTime,
+        };
+
+        console.log(newBookmark);
+
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)),
+        });
+    };
 })();
+
+const getTime = (t) => {
+    var date = new Date(0);
+    date.setSeconds(t);
+
+    return date.toISOString().substr(11, 8);
+};
